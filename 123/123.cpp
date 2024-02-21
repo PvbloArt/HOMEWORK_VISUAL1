f8a6c1f55fbe88f78b21d1ad7afd8b8b63093763
Artyom Pvblo <sotr.pvblo@mail.ru>
2024-02-21 20:35:50 +0300

    Добавьте файлы проекта.

---
+++ 123/123.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include "Helper.h"
+
+int main() {
+    int a = 3;
+    int b = 10;
+
+    int result = squareSum(a, b);
+
+    std::cout << "Sum " << a << " and " << b << " Summa =" << result << std::endl;
+
+    return 0;
+}
---
+++ 123/Helper.h
@@ -0,0 +1,7 @@
+
+#define Helper_h
+
+int squareSum(int a, int b) {
+    int sum = a + b;
+    return sum * sum;
+}
